7b0c5d5f7eb34d01557b989b1b715c189aa8d98a
lab05l <pedroos.leal07@gmail.com>
2025-10-07 21:57:16 -0300

    Ajustes na calculadora e nos testes

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -2,3 +2,9 @@ from src.calculadora import soma
 
 def test_soma():
   assert soma(2 , 3) == 5
+  
+def test_subtracao():
+  assert subtracao(5,3) == 2
+
+def test_multiplicacao():
+  assert multiplicacao(5,3) ==15   
